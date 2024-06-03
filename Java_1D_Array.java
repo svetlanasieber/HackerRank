d7a1cdc8a92f8cb16d14a0b3456934ddd209bbf6
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-06-03 02:08:44 +0200

    Add files via upload

---
+++ Java_1D_Array.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Java_1D_Array {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int n = scan.nextInt();
+        int [] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            int currentNumber = scan.nextInt();
+            a[i] = currentNumber;
+        }
+        scan.close();
+
+        for (int i = 0; i < a.length; i++) {
+            System.out.println(a[i]);
+        }
+    }
+}
